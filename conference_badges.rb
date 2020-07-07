2ebc611e3722ebf59df1fc95c9ef1de70b7d9634
Joseph cohen <jojocohen57@gmail.com>
2020-07-07 19:24:47 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,8 +5,10 @@ end
 def batch_badge_creator(array)
    new_arr = []
    array.each do |name|
-    "Hello, my name is #{name}."
+    variable = "Hello, my name is #{name}."
+    new_arr << variable
    end
+   return new_arr
 end  
 
 def assign_rooms(speakers)
