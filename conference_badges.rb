c91515b68d91e6085101d6dd16b7040eaf652bfb
Joseph cohen <jojocohen57@gmail.com>
2020-07-07 19:43:55 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,6 +21,10 @@ def assign_rooms(speakers)
 end   
 
 def printer(attendees)
-  puts batch_badge_creator(attendees)
+  d = false
+  batch_badge_creator(attendees).each do |attendee|
+    d = true
+    if 
+    puts attendee
   puts assign_rooms(attendees)
 end  
